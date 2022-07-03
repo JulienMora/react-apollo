8d83f6fe09d362761d0519c75aa6d54ea21777f3
Julien Moratalla <julien.moratalla@gmail.com>
2022-07-03 21:29:20 +0200

    Update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
+
+// UI
 import './App.css';
+import { Grid, Card, CardContent, Typography } from '@mui/material';
 
 // GRAPHQL IMPORTS
 import { GET_LAUNCHES } from "./queries";
 import { useQuery } from '@apollo/client';
 
 
-function App() {
+const App = () => {
     const { loading, error, data } = useQuery(GET_LAUNCHES);
 
     if (loading) return <p>Loading...</p>;
@@ -17,11 +20,19 @@ function App() {
       <div>
           <h2>Apollo - SpaceX Missions 🚀</h2>
 
-          {data.launches.map((item: any, index: number) => (
-              <div key={index}>
-                  <h3>{item.mission_name}</h3>
-              </div>
-          ))}
+          <Grid container spacing={2}>
+              {data.launches.map((item: any, index: number) => (
+                  <Grid key={index} item xs={4}>
+                      <Card>
+                          <CardContent>
+                              <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                  {item.mission_name}
+                              </Typography>
+                          </CardContent>
+                      </Card>
+                  </Grid>
+              ))}
+          </Grid>
       </div>
   );
 }
